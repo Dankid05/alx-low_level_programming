ea1bd070947bc3b9a9248ba9417c6a8259b4ec74
Dankid05 <akinrinadedaniel66@gmail.com>
2022-09-08 19:16:37 -0700

    my task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
-#include <unistd.h>
+#include <stdio.h>                                
 /**
- * main - Entry point
- * Return:1
- */
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
-
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
